d18386425cfa920c0fb922cbab643a392928c507
Tanel Põld <tanel@brightside.ee>
2018-11-26 14:15:20 +0800

    Remove sys_language_overlay tables

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,8 +8,3 @@ CREATE TABLE pages (
 CREATE TABLE sys_file_reference (
  tx_macrotemplate_headerimage_placement tinytext,
 );
-
-CREATE TABLE pages_language_overlay (
-	tx_macrotemplate_headerimage int(11) unsigned DEFAULT '0',
-  tx_macrotemplate_icon int(11) unsigned DEFAULT '0',
-);
