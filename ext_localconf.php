e8bcb31508ead8ae373ce5f4d3e3bda780c40935
Tanel Põld <tanel@brightside.ee>
2019-01-04 14:32:13 +0800

    move isLoaded TS constant creation

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,12 @@
 
 	$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['macrotemplate'] = 'EXT:macrotemplate/Configuration/RTE/Default.yaml';
 	$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'].= ',tx_macrotemplate_headerimage,uid';
+	if (TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('personnel')){
+  	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+  		macrotemplate.personnelIsLoaded = 1
+  	');
+  } else {
+  	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+  		macrotemplate.personnelIsLoaded = 0
+  	');
+  }
