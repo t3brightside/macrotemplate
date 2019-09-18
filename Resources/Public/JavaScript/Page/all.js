0466ccf91bb28385cfad43a874a80fc881958bbb
Tanel Põld <tanel@brightside.ee>
2019-09-18 14:38:00 +0300

    form validation and css

--- Resources/Public/JavaScript/Page/all.js
+++ Resources/Public/JavaScript/Page/all.js
@@ -63,3 +63,19 @@ $(document).ready(function() {
       }
     }
 });
+
+$(document).ready(function() {
+    /* html5 based validation class */
+    $("form .btn").click( function(){
+        $("select, input, textarea").each(function(){
+            if($(this).is(":invalid")) {
+                $(this).addClass("error")
+            }
+        })
+    });
+    $('select, input, textarea').on('blur', function(){
+        if($(this).is(":valid") && $(this).hasClass('error')) {
+            $(this).removeClass("error")
+        }
+    });
+});
